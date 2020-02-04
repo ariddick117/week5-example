7bab964ec6cf0313f87b8d9e3ec779d9c904a687
Alec <a_riddick@fanshaweonline.ca>
2020-02-04 17:44:16 -0500

    Added Routes and a User Login

---
+++ js/modules/ErrorComponent.js
@@ -0,0 +1,7 @@
+export default {
+    template: `<h1>Ah Ah Ah! You didn't use the magic word! Ah ah ah!</h1>`,
+
+    created: function() {
+        console.log('our error component rendered');
+    },
+}
